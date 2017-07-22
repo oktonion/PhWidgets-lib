27c4109d4ec5417bd827f79e60f069097da69994
oktonion <0oktonion0@gmail.com>
2017-07-23 00:21:31 +0300

    events are getting smarter
    events now can be bound to any callback
    also fixed dos line endings

--- src/service/phevent.hpp
+++ src/service/phevent.hpp
@@ -4,6 +4,7 @@
 #include <photon/PtWidget.h>
 
 #include <set>
+#include <map>
 
 namespace phevents
 {
@@ -48,24 +49,19 @@ namespace phevents
 		};
 	}
 
-	struct phevent
+	class phevent
 	{
-		typedef PtCallback_t ph_callback_t;
-
-		template<class ParentT, typename detail::get_parent_func<ph_callback_t, ParentT>::adder_t Adder, typename detail::get_parent_func<ph_callback_t, ParentT>::remover_t Remover = detail::get_parent_func<ph_callback_t, ParentT>::default_remover()>
-		class bind
-		{
-			typedef typename detail::get_parent_func<ph_callback_t, ParentT>::value_t value_t;
-
 			class callback_container
 			{
 				typedef int(*callback_t)( PtWidget_t *, void *, PtCallbackInfo_t * );
-				typedef int(*callback_proxy_t)( callback_container *, PtWidget_t *, void *, PtCallbackInfo_t * );
+				typedef int(*callback_proxy_t)(const callback_container *, PtWidget_t *, void *, PtCallbackInfo_t * );
 
 			public:
-
-				callback_container(callback_t callback):
-					_callback(callback)
+				
+				template<class Param1T, class Param2T, class Param3T>
+				callback_container(int(*callback)( Param1T, Param2T, Param3T)):
+					_callback(reinterpret_cast<callback_t>(callback)),
+					_proxy_call(&callback_container::proxy_call<Param1T, Param2T, Param3T>)
 				{}
 
 				template<class Param1T, class Param2T, class Param3T>
@@ -74,32 +70,109 @@ namespace phevents
 					_proxy_call = &callback_container::proxy_call<Param1T, Param2T, Param3T>;
 				}
 
-				inline int operator()(PtWidget_t *p1, void *p2, PtCallbackInfo_t *p3)
+				inline int operator()(PtWidget_t *p1, void *p2, PtCallbackInfo_t *p3) const
 				{
 					return _proxy_call(this, p1, p2, p3);
 				}
 
-				inline bool operator<(const callback_container &rhs) { return (_callback < rhs._callback || _proxy_call < rhs._proxy_call); }
+				inline bool operator<(const callback_container &rhs) const { return (_callback < rhs._callback || _proxy_call < rhs._proxy_call); }
 
 			private:
 				callback_t _callback;
 				callback_proxy_t _proxy_call;
 
 				template<class Param1T, class Param2T, class Param3T>
-				static int proxy_call(callback_container *container, PtWidget_t *p1, void *p2, PtCallbackInfo_t *p3) 
+				static int proxy_call(const callback_container *container, PtWidget_t *p1, void *p2, PtCallbackInfo_t *p3) 
 				{ 
 					return reinterpret_cast<int(*)(Param1T, Param2T, Param3T)>(container->_callback)(p1, p2, p3); 
 				}
 			};
 
-			static std::set<callback_container> &repo() { static std::set<callback_container> repo; return repo; }
+	public:
+		class ph_callback_t
+		{
+			class cb_with_data
+			{
+				typedef std::set<callback_container> callback_repo_t;
 
+			public:
+
+				cb_with_data(callback_container cb, void *dt = 0)
+				{
+					std::pair<callback_repo_t::iterator, bool> res = cb_with_data::repo().insert(cb);
+
+					callback = &(*res.first);
+					data = dt;
+				}
+
+				inline int operator()(PtWidget_t *p1, PtCallbackInfo_t *p3) const
+				{
+					return (*callback)(p1, data, p3);
+				}
+
+				inline bool operator<(const cb_with_data &rhs) const { return callback < rhs.callback || data < rhs.data; }
+
+			private:
+				const callback_container *callback;
+				void *data;
+
+				static callback_repo_t &repo() { static callback_repo_t repo; return repo; }
+			};
+			
+			typedef std::set<cb_with_data> cb_data_repo_t;
+
+			static cb_data_repo_t &repo() { static cb_data_repo_t repo; return repo; }
+			
 			static int proxy_call(PtWidget_t *p1, void *p2, PtCallbackInfo_t *p3)
 			{
-				callback_container &cbc = *reinterpret_cast<callback_container*>(p2);
+				cb_with_data &cbwd = *reinterpret_cast<cb_with_data*>(p2);
 
-				return cbc(p1, 0, p3);
+				cb_data_repo_t::iterator it = repo().find(cbwd);
+
+				if(repo().end() == it)
+					return 0;
+
+				return cbwd(p1, p3);
 			}
+
+		public:
+			ph_callback_t(const PtCallback_t &callback):
+				_callback(callback)
+			{}
+			
+			template<class Param1T, class Param2T, class Param3T>
+			ph_callback_t(int(*value)(Param1T, Param2T, Param3T))
+			{
+				std::pair<cb_data_repo_t::iterator, bool> res = repo().insert(cb_with_data(value));
+
+				_callback.event_f = &proxy_call;
+				_callback.data = &(*(res.first));
+			}
+
+			template<class Param1T, class Param2T, class Param3T>
+			ph_callback_t(int(*value)(Param1T, Param2T, Param3T), Param2T &data)
+			{
+				std::pair<cb_data_repo_t::iterator, bool> res = repo().insert(cb_with_data(value, data));
+
+				_callback.event_f = &proxy_call;
+				_callback.data = &(*(res.first));
+			}
+
+			~ph_callback_t()
+			{
+				//do NOTHING!
+			}
+
+			inline operator PtCallback_t() const{ return _callback; }
+			
+		private:
+			PtCallback_t _callback;
+		};
+
+		template<class ParentT, typename detail::get_parent_func<ph_callback_t, ParentT>::adder_t Adder, typename detail::get_parent_func<ph_callback_t, ParentT>::remover_t Remover = detail::get_parent_func<ph_callback_t, ParentT>::default_remover()>
+		class bind
+		{
+			typedef typename detail::get_parent_func<ph_callback_t, ParentT>::value_t value_t;
 
 		public:
 			bind(ParentT *parent) :
@@ -114,30 +187,6 @@ namespace phevents
 			inline void remove(value_t value)
 			{
 				(_obj->*Remover)(value);
-			}
-			
-			template<class Param1T, class Param2T, class Param3T>
-			inline void add(int(*value)(Param1T, Param2T, Param3T))
-			{
-				std::pair<typename std::set<callback_container>::iterator, bool> res = repo().insert(callback_container(value));
-
-				PtCallback_t proxy;
-				proxy.event_f = &bind::proxy_call;
-				proxy.data = &(*(res.first));
-
-				(_obj->*Adder)(proxy);
-			}
-
-			template<class Param1T, class Param2T, class Param3T>
-			inline void remove(int(*value)(Param1T, Param2T, Param3T))
-			{
-				std::pair<typename std::set<callback_container>::iterator, bool> res = repo().insert(callback_container(value));
-
-				PtCallback_t proxy;
-				proxy.event_f = &bind::proxy_call;
-				proxy.data = &(*(res.first));
-
-				(_obj->*Remover)(proxy);
 			}
 
 			inline void operator+=(value_t value)
