dc5a201b89e885894917594664bea605652a4ef8
oktonion <0oktonion0@gmail.com>
2017-10-18 17:26:05 +0300

    + bitmask |

--- src/service/bitmask.hpp
+++ src/service/bitmask.hpp
@@ -11,8 +11,9 @@ namespace cppbitmasks
 			_val(MaskT())
 		{
 		}
-		bitmask(const MaskT &val) :
-			_val(val)
+
+		bitmask(const FlagT &flag) :
+			_val(flag)
 		{
 		}
 
@@ -21,38 +22,100 @@ namespace cppbitmasks
 		{
 		}
 
-		bool test(const FlagT &flag) const
+		inline bool test(const FlagT &flag) const
 		{
 			return 0 != (_val & flag);
 		}
 
-		bitmask& set(const FlagT &flag)
+		inline bool test(const bitmask &mask) const
+		{
+			return 0 != (_val & mask);
+		}
+
+		inline bitmask& set(const FlagT &flag)
 		{
 			_val |= flag;
 			return *this;
 		}
 
-		bitmask& reset(const FlagT &flag)
+		inline bitmask& set(const bitmask &mask)
+		{
+			_val |= mask._val;
+			return *this;
+		}
+
+		inline bitmask& reset(const FlagT &flag)
 		{
 			_val &= ~flag;
 			return *this;
 		}
 
-		bitmask& operator=(const MaskT &val)
+		inline bitmask& reset(const bitmask &mask)
 		{
-			_val = val;
+			_val &= ~mask._val;
 			return *this;
 		}
 
-		bitmask& operator=(const bitmask &other)
+		inline bitmask& keep(const FlagT &flag)
+		{
+			_val &= flag;
+			return *this;
+		}
+
+		inline bitmask& keep(const bitmask &mask)
+		{
+			_val &= mask._val;
+			return *this;
+		}
+
+		inline bitmask& flip(const FlagT &flag)
+		{
+			_val ^= flag;
+			return *this;
+		}
+
+		inline bitmask& flip(const bitmask &mask)
+		{
+			_val ^= mask._val;
+			return *this;
+		}
+
+		inline bitmask& operator|(const FlagT &flag)
+		{
+			return set(flag);
+		}
+
+		inline bitmask operator|(const bitmask &mask) const
+		{
+			return _val | mask._val;
+		}
+
+		inline bitmask& operator=(const bitmask &other)
 		{
 			_val = other._val;
 			return *this;
 		}
 
+		inline bitmask& operator=(const FlagT &flag)
+		{
+			_val = flag;
+			return *this;
+		}
+
+		inline operator const MaskT&() const
+		{
+			return _val;
+		}
+
 	private:
 		MaskT _val;
 	};
+
+	template<class MaskT, class FlagT>
+	inline bitmask<MaskT, FlagT>  &operator|(const FlagT &flag1, const bitmask<MaskT, FlagT> &flag2)
+	{
+		return flag2 | flag1;
+	}
 }
 
 #endif
