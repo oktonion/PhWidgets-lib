dad1b5de8ab7aed8866e4c0452aba697020fc6a2
Alexander <oktonion.work@gmail.com>
2019-01-29 18:01:31 +0300

    +tests for FontDef

--- tests/microGUI/font_def.cpp
+++ tests/microGUI/font_def.cpp
@@ -11,5 +11,11 @@ TEST_CASE("Testing FontDef class"){
     
     CHECK_THROWS(FontDef fd(0)); // should throw std::invalid_argument
 
-    FontDef fd(0);
+    FontDef fd(GenericFontFamilies::Serif, 10);
+
+    CHECK(fd.Name != "");
+    CHECK(fd.Size == 10);
+    CHECK(fd.Italic == false);
+    CHECK(fd.Bold == false);
+    CHECK(fd.Family == GenericFontFamilies::Serif);
 }
