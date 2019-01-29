f64b3bf728bec4ad6d064ba8098c8814474b8922
Alexander <0oktonion0@gmail.com>
2019-01-29 23:28:01 +0300

    moar tests for FontDef

--- tests/microGUI/font_def.cpp
+++ tests/microGUI/font_def.cpp
@@ -31,4 +31,17 @@ TEST_CASE("Testing FontDef class"){
         CHECK(fd2 == fd);
         CHECK(fd < fd2 == false);
     }
+
+    SUBCASE("Testing other FontDef"){
+
+        FontDef fd2(GenericFontFamilies::Serif, 13, FontStyle::Bold | FontStyle::Italic);
+
+        CHECK("" != fd2.Name);
+        CHECK(13 == fd2.Size);
+        CHECK(true == fd2.Italic);
+        CHECK(true = fd2.Bold);
+        CHECK(GenericFontFamilies::Serif == fd2.Family);
+        CHECK(fd2 != fd);
+        CHECK(fd < fd2 == true);
+    }
 }
