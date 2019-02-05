fbceebc616840ed3bc382d79160d9419a13718ef
Alexander <oktonion.work@gmail.com>
2019-02-05 11:05:13 +0300

    moar tests for InstalledFontCollection

--- tests/microGUI/font_collection.cpp
+++ tests/microGUI/font_collection.cpp
@@ -13,6 +13,9 @@ TEST_CASE("Testing FontCollections classes"){
     InstalledFontCollection ifc;
 
     CHECK(ifc.Families.size() != 0);
-
     CHECK(ifc.Families().size() == ifc.Families.size());
+    CHECK(ifc.Families.begin() != ifc.Families.end());
+    CHECK(ifc.Families.begin() == ifc.Families().begin());
+    CHECK(ifc.Families().end() == ifc.Families.end());
+
 }
