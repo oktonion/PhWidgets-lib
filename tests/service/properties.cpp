e07b40bd64746816adea8b12592a964b8da2aaee
Alexander <oktonion.work@gmail.com>
2019-02-06 11:46:00 +0300

    more properties tests

--- tests/service/properties.cpp
+++ tests/service/properties.cpp
@@ -3,28 +3,66 @@
 #include <service/property.hpp>
 
 
-TEST_CASE("Testing cppproperties") {
-    
+
+
+TEST_CASE("Testing properties for simple types"){
+
     using namespace cppproperties;
 
-    SUBCASE("Testing properties for simple types"){
-        {
-            property<int> prop_int;
-
-            prop_int = 11;
-
-            CHECK(prop_int.get() == 11);
-            CHECK(11 == prop_int);
-            CHECK(prop_int == 11);
-        }
-
-        {
-            const property<int> prop_int(42);
-
-            CHECK(prop_int.get() == 42);
-            CHECK(42 == prop_int);
-            CHECK(prop_int == 42);
-        }
-    }
+    SUBCASE("Testing rw property"){
     
+        property<int> prop_int;
+
+        prop_int = 11;
+
+        CHECK(prop_int.get() == 11);
+        CHECK(11 == prop_int);
+        CHECK(prop_int == 11);
+
+        int val = 42;
+        prop_int.set(val);
+
+        CHECK(prop_int.get() == val);
+        CHECK(val == prop_int);
+        CHECK(prop_int == val);
+    }
+
+    SUBCASE("Testing const rw property"){
+
+        const property<int> prop_int(42);
+
+        CHECK(prop_int.get() == 42);
+        CHECK(42 == prop_int);
+        CHECK(prop_int == 42);
+    }
+
+    SUBCASE("Testing ro property of const"){
+
+        property<const int> prop_int(42);
+
+        CHECK(prop_int.get() == 42);
+        CHECK(42 == prop_int);
+        CHECK(prop_int == 42);
+    }
+
+    SUBCASE("Testing ro property"){
+
+        property<int, property<>::ro> prop_int(42);
+
+        CHECK(prop_int.get() == 42);
+        CHECK(42 == prop_int);
+        CHECK(prop_int == 42);
+
+        property<const int> 
+            prop_cint(42),
+            prop_cint2(prop_int + 1);
+
+        CHECK(prop_cint == prop_int);
+        CHECK(prop_int == prop_cint);
+        CHECK_FALSE(prop_int > prop_cint);
+
+        CHECK(prop_cint2 > prop_int);
+        CHECK(prop_cint2 != prop_int);
+        CHECK(prop_int != prop_cint2);
+    }
 }
