4e8aeb65b67ff24a788d1528fd95207298ccc827
Alexander <0oktonion0@gmail.com>
2019-05-22 19:23:04 +0300

    + Widget::Enabled property test

---
+++ tests/microGUI/prop_enabled.cpp
@@ -0,0 +1,32 @@
+#include "./phwidgets_init.cpp"
+
+#include "./testsuit.h"
+
+#include <Widget.h>
+#include <photon/PtWindow.h>
+
+
+TEST_CASE("Testing Widget::Enabled property"){
+
+    using namespace PhWidgets;
+    
+    SUBCASE("Windows Enabled test"){
+        REQUIRE(PhWidgetsGetWidget<&PtWindow>());
+
+        Widget widget(PhWidgetsGetWidget<&PtWindow>());
+        
+        CHECK(widget.Enabled == true);
+        widget.Enabled = false;
+        CHECK(false == widget.Enabled);
+    }
+
+    SUBCASE("Button Enabled test"){
+        REQUIRE(PhWidgetsGetWidget<&PtButton>());
+        
+        Button button(PhWidgetsGetWidget<&PtButton>());
+        
+        CHECK(button.Enabled == true);
+        button.Enabled = false;
+        CHECK(false == button.Enabled);
+    }
+}
