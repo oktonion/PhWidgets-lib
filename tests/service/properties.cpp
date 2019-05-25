f3ee6d4f7a05ef2db15848f04a09defa80d5fa43
Alexander <0oktonion0@gmail.com>
2019-05-25 14:24:36 +0300

    Update properties.cpp

--- tests/service/properties.cpp
+++ tests/service/properties.cpp
@@ -82,7 +82,7 @@ TEST_CASE("Testing properties for simple types"){
 
         prop_int.set(10);
         CHECK(prop_int == prop_int);
-        CHECK_EQ(10, prop_int);
+        //CHECK_EQ(10, prop_int);
         CHECK(prop_int == 10);
         CHECK_EQ(prop_int, 10);
         prop_int.set(convertable<int>());
