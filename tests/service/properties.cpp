c7fddc8216bf541ccb5f198969eabf88021a6453
Alexander <0oktonion0@gmail.com>
2019-05-25 22:18:27 +0300

    Update properties.cpp

--- tests/service/properties.cpp
+++ tests/service/properties.cpp
@@ -20,7 +20,9 @@ struct property_container
     property_container(T value_ = T()):
         _value(value_),
         value(this),
-        const_value(this)
+        const_value(this),
+        ro_value(this),
+        wo_value(this)
     { }
 
 private:
@@ -36,42 +38,48 @@ public:
     typename
     cppproperties::property<T>::
         template bind<property_container, &property_container::getter, &property_container::setter> const const_value;
+
+    typename
+    cppproperties::property<T, cppproperties::property<>::ro>::
+        template bind<property_container, &property_container::getter> ro_value;
+
+    typename
+    cppproperties::property<T, cppproperties::property<>::wo>::
+        template bind<property_container, &property_container::setter> wo_value;
 };
 
+template<class PropertyT>
+void property_test_subcase1(PropertyT &property, const char *message)
+{
+    SUBCASE(message) {
 
-TEST_CASE("Testing properties for simple types"){
+        property = 11;
 
-    using namespace cppproperties;
-
-    SUBCASE("Testing rw property"){
-    
-        property<int> prop_int;
-
-        prop_int = 11;
-
-        CHECK(prop_int.get() == 11);
-        CHECK(11 == prop_int);
-        CHECK(prop_int == 11);
+        CHECK(property.get() == 11);
+        CHECK(11 == property);
+        CHECK(property == 11);
 
         int val = 42;
-        prop_int.set(val);
+        property.set(val);
 
-        CHECK(prop_int.get() == val);
-        CHECK(val == prop_int);
-        CHECK(prop_int == val);
+        CHECK(property.get() == val);
+        CHECK(val == property);
+        CHECK(property == val);
 
-        CHECK_FALSE(prop_int != val);
-        CHECK_FALSE(val != prop_int);
-        CHECK_FALSE(prop_int != prop_int);
+        CHECK_FALSE(property != val);
+        CHECK_FALSE(val != property);
+        CHECK_FALSE(property != property);
 
-        CHECK_FALSE(val < prop_int);
-        CHECK_FALSE(prop_int < val);
-        CHECK_FALSE(prop_int < prop_int);
+        CHECK_FALSE(val < property);
+        CHECK_FALSE(property < val);
+        CHECK_FALSE(property < property);
     }
+}
 
-    SUBCASE("Testing const rw property"){
-
-        const property<int> prop_int(42);
+template<class PropertyT>
+void property_test_subcase2(PropertyT &property, const char *message)
+{
+    SUBCASE(message) {
 
         CHECK(prop_int.get() == 42);
         CHECK(42 == prop_int);
@@ -85,10 +93,12 @@ TEST_CASE("Testing properties for simple types"){
         CHECK_FALSE(prop_int < 42);
         CHECK_FALSE(prop_int < prop_int);
     }
+}
 
-    SUBCASE("Testing ro property of const"){
-
-        property<const int> prop_int(42);
+template<class PropertyT>
+void property_test_subcase3(PropertyT &property, const char *message)
+{
+    SUBCASE(message) {
 
         CHECK(prop_int.get() == 42);
         CHECK(42 == prop_int);
@@ -104,10 +114,12 @@ TEST_CASE("Testing properties for simple types"){
         CHECK_FALSE(prop_int < convertable<int>());
         CHECK_FALSE(prop_int < prop_int);
     }
+}
 
-    SUBCASE("Testing ro property"){
-
-        property<int, property<>::ro> prop_int(42);
+template<class PropertyT>
+void property_test_subcase4(PropertyT &property, const char *message)
+{
+    SUBCASE(message) {
 
         CHECK(prop_int.get() == 42);
         CHECK(42 == prop_int);
@@ -125,10 +137,12 @@ TEST_CASE("Testing properties for simple types"){
         CHECK(prop_cint2 != prop_int);
         CHECK(prop_int != prop_cint2);
     }
+}
 
-    SUBCASE("Testing wo property"){
-
-        property<int, property<>::wo> prop_int(10);
+template<class PropertyT>
+void property_test_subcase5(PropertyT &property, const char *message)
+{
+    SUBCASE(message) {
 
         prop_int.set(10);
         CHECK(prop_int == prop_int);
@@ -137,6 +151,42 @@ TEST_CASE("Testing properties for simple types"){
         CHECK_EQ(prop_int, 10);
         prop_int.set(convertable<int>());
         CHECK(prop_int != 10);
+    }
+}
+
+
+TEST_CASE("Testing properties for simple types"){
+
+    using namespace cppproperties;
+
+    {
+        property<int> prop_int;
+
+        property_test_subcase1(prop_int, "Testing rw property");
+    }
+
+    {
+        const property<int> prop_int(42);
+
+        property_test_subcase2(prop_int, "Testing const rw property");
+    }
+
+    {
+        property<const int> prop_int(42);
+
+        property_test_subcase3(prop_int, "Testing ro property of const");
+    }
+
+    {
+        property<int, property<>::ro> prop_int(42);
+
+        property_test_subcase4(prop_int, "Testing ro property");
+    }
+
+    {
+        property<int, property<>::wo> prop_int(10);
+
+        property_test_subcase5(prop_int, "Testing wo property");
     }
 
     SUBCASE("Testing mixed property"){
@@ -216,48 +266,36 @@ TEST_CASE("Testing properties for simple types"){
         //CHECK(prop_rw < prop_wo);
     }
 
-
-    SUBCASE("Testing rw contained property"){
+    {
         property_container<int> prop_cont;
 
         prop_cont.value = 11;
 
-        CHECK(prop_cont.value.get() == 11);
-        CHECK(11 == prop_cont.value);
-        CHECK(prop_cont.value == 11);
-
-        int val = 42;
-        prop_cont.value.set(val);
-
-        CHECK(prop_cont.value.get() == val);
-        CHECK(val == prop_cont.value);
-        CHECK(prop_cont.value == val);
-
-        CHECK_FALSE(prop_cont.value != val);
-        CHECK_FALSE(val != prop_cont.value);
-        CHECK_FALSE(prop_cont.value != prop_cont.value);
-
-        CHECK_FALSE(val < prop_cont.value);
-        CHECK_FALSE(prop_cont.value < val);
-        CHECK_FALSE(prop_cont.value < prop_cont.value);
+        property_test_subcase1(prop_cont.value, "Testing rw contained property");
     }
 
-    SUBCASE("Testing const rw contained property"){
+    {
 
-        property_container<int> prop_cont;
+        property_container<int> prop_cont(42);
 
-        prop_cont.value = 42;
+        property_test_subcase2(prop_cont.const_value, "Testing const rw contained property");
+    }
 
-        CHECK(prop_cont.const_value.get() == 42);
-        CHECK(42 == prop_cont.const_value);
-        CHECK(prop_cont.const_value == 42);
+    {
+        property_container<const int> prop_cont(42);
 
-        CHECK_FALSE(prop_cont.const_value != 42);
-        CHECK_FALSE(42 != prop_cont.const_value);
-        CHECK_FALSE(prop_cont.const_value != prop_cont.const_value);
+        property_test_subcase3(prop_cont.value, "Testing ro contained property of const");
+    }
 
-        CHECK_FALSE(42 < prop_cont.const_value);
-        CHECK_FALSE(prop_cont.const_value < 42);
-        CHECK_FALSE(prop_cont.const_value < prop_cont.const_value);
+    {
+        property_container<int> prop_cont(42);
+
+        property_test_subcase4(prop_cont.ro_value, "Testing ro contained property");
+    }
+
+    {
+        property_container<int> prop_cont(10);
+
+        property_test_subcase5(prop_cont.wo_value, "Testing wo property");
     }
 }
