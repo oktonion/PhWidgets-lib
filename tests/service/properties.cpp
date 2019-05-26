2f4f9f6f181cb6736a9c0a257fd1affeefea4972
Alexander <0oktonion0@gmail.com>
2019-05-26 22:22:30 +0300

    Update properties.cpp

--- tests/service/properties.cpp
+++ tests/service/properties.cpp
@@ -410,10 +410,10 @@ TEST_CASE("Testing properties for class types"){
         /*CHECK*/(prop_vint >= val);
 
         /*CHECK_FALSE*/(prop_vint == convertable<std::vector<int>/**/>());
-        /*CHECK*/(prop_vint != convertable<std::vector<int>/**/>());
-        /*CHECK_FALSE*/(prop_vint < convertable<std::vector<int>/**/>());
-        /*CHECK*/(prop_vint > convertable<std::vector<int>/**/>());
-        /*CHECK_FALSE*/(prop_vint <= convertable<std::vector<int>/**/>());
-        /*CHECK*/(prop_vint >= convertable<std::vector<int>/**/>());
+        ///*CHECK*/(prop_vint != convertable<std::vector<int>/**/>());
+        ///*CHECK_FALSE*/(prop_vint < convertable<std::vector<int>/**/>());
+        ///*CHECK*/(prop_vint > convertable<std::vector<int>/**/>());
+        ///*CHECK_FALSE*/(prop_vint <= convertable<std::vector<int>/**/>());
+        ///*CHECK*/(prop_vint >= convertable<std::vector<int>/**/>());
     }
 }
