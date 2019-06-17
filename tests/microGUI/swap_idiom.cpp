6ecbb31ed0b971185b0d4aa34169a1a9aacaddbf
Alexander <oktonion.work@gmail.com>
2019-06-17 16:12:52 +0300

    Update swap_idiom.cpp

--- tests/microGUI/swap_idiom.cpp
+++ tests/microGUI/swap_idiom.cpp
@@ -3,31 +3,35 @@
 #include "./testsuit.h"
 
 #include <Widget.h>
+#include <Button.h>
 #include <photon/PtWindow.h>
+#include <photon/PtButton.h>
 
 
 TEST_CASE("Testing swap idiom of Widget") {
     REQUIRE(PhWidgetsGetWidget<&PtWindow>());
+    REQUIRE(PhWidgetsGetWidget<&PtButton>());
     
     using namespace PhWidgets;
 
     Widget 
-        widget1(PhWidgetsGetWidget<&PtWindow>()),
-        widget2(PhWidgetsGetWidget<&PtWindow>());
+        widget1(PhWidgetsGetWidget<&PtWindow>());
+    Button
+        widget2(PhWidgetsGetWidget<&PtButton>());
 
     Widget tmp = widget1;
 
     CHECK(tmp == widget1);
-    CHECK(widget == tmp);
+    CHECK(widget1 == tmp);
     CHECK(widget2 != tmp);
     CHECK(tmp != widget2);
     CHECK(widget2 != widget1);
     CHECK(widget1 != widget2);
 
-    widget2.swap(wiget1);
+    widget2.swap(widget1);
 
     CHECK(tmp != widget1);
-    CHECK(widget != tmp);
+    CHECK(widget1 != tmp);
     CHECK(widget2 == tmp);
     CHECK(tmp == widget2);
     CHECK(widget2 != widget1);
