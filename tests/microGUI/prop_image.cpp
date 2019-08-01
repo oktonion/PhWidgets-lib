563262c5eef66991130af43a19bba43012c187d7
Alexander <0oktonion0@gmail.com>
2019-08-02 01:10:03 +0300

    Update prop_image.cpp
    Images of all types checks

--- tests/microGUI/prop_image.cpp
+++ tests/microGUI/prop_image.cpp
@@ -14,60 +14,86 @@ TEST_CASE("Testing Label::Image property") {
 
     Label label(PhWidgetsGetWidget<&PtLabel>());
 
+    label.Type = LabelType::Image;
+
+    REQUIRE(label.Type == LabelType::Image);
+
     CHECK_THROWS(Image::FromFile("there/is/no/such/file.porn"));
 
-    MESSAGE("Trying to load beautiful Lenna into PhWidgets::Drawing::Image");
+    SUBCASE("BMP image test: Trying to load beautiful Lenna into PhWidgets::Drawing::Image"){
 
-    SUBCASE("BMP image test"){
+        CHECK_NOTHROW({
+        	
+        	label.Image = Image::FromFile("./tests/service/resources/lenna.bmp");
+        });
 
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/lenna.bmp"));
+        CHECK(label.Image());
 
         CHECK(label.Image().Width == 512);
         CHECK(512 == label.Image().Height);
     }
 
-    SUBCASE("PNG image test"){
+    SUBCASE("PNG image test: Trying to load beautiful Lenna into PhWidgets::Drawing::Image"){
 
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/lenna.png"));
+        CHECK_NOTHROW({
+
+            label.Image = Image::FromFile("./tests/service/resources/lenna.png");
+        });
+
+        CHECK(label.Image());
 
         CHECK(label.Image().Width == 746);
         CHECK(900 == label.Image().Height);
     }
 
-    MESSAGE("Trying to load sexy banana into PhWidgets::Drawing::Image");
+    SUBCASE("JPG image test: Trying to load sexy banana into PhWidgets::Drawing::Image"){
 
-    SUBCASE("JPG image test"){
+        CHECK_NOTHROW({
 
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/banana.jpg"));
+            label.Image = Image::FromFile("./tests/service/resources/banana.jpg");
+        });
 
-        CHECK(label.Image().Width == 800);
-        CHECK(448 == label.Image().Height);
-    }
-
-    SUBCASE("GIF image test"){
-
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/banana.gif"));
+        CHECK(label.Image());
 
         CHECK(label.Image().Width == 800);
         CHECK(448 == label.Image().Height);
     }
 
-    MESSAGE("Trying to load colorful marbles into PhWidgets::Drawing::Image");
+    SUBCASE("GIF image test: Trying to load sexy banana into PhWidgets::Drawing::Image"){
 
-    SUBCASE("PCX image test"){
+        CHECK_NOTHROW({
+            
+            label.Image = Image::FromFile("./tests/service/resources/banana.gif");
+        });
 
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/marbles.pcx"));
+        CHECK(label.Image());
+        
+        CHECK(label.Image().Width == 800);
+        CHECK(448 == label.Image().Height);
+    }
 
+    SUBCASE("PCX image test: Trying to load colorful marbles into PhWidgets::Drawing::Image"){
+
+        CHECK_NOTHROW({
+
+            label.Image = Image::FromFile("./tests/service/resources/marbles.pcx");
+        });
+
+        CHECK(label.Image());
+        
         CHECK(label.Image().Width == 1419);
         CHECK(1001 == label.Image().Height);
     }
 
-    MESSAGE("Trying to load gorgeous girl into PhWidgets::Drawing::Image");
+    SUBCASE("SGI image test: Trying to load gorgeous girl into PhWidgets::Drawing::Image"){
 
-    SUBCASE("SGI image test"){
+        CHECK_NOTHROW({
 
-        CHECK_NOTHROW(label.Image = Image::FromFile("./../service/resources/girl.sgi"));
+            label.Image = Image::FromFile("./tests/service/resources/girl.sgi");
+        });
 
+        CHECK(label.Image());
+        
         CHECK(label.Image().Width == 768);
         CHECK(1024 == label.Image().Height);
     }
