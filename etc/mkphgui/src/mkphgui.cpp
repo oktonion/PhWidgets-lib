b3f19a3a727d1a9008c73828be3348f78e7c760b
Alexander <0oktonion0@gmail.com>
2019-08-05 23:51:49 +0300

    Update mkphgui.cpp

--- etc/mkphgui/src/mkphgui.cpp
+++ etc/mkphgui/src/mkphgui.cpp
@@ -21,30 +21,31 @@ int main(int argc, const char* argv[])
 
     std::fstream fs;
     std::map<int, ApDBWidgetInfo_t> widgets;
-    std::map<int, std::vector<ApDBWidgetInfo_t>/**/> hierarchy;
+    std::map<int, std::vector<int>/**/> hierarchy;
 
     for (int i = 0; ApGetDBWidgetInfo(dbase, i, &wi); ++i)
     {
-        printf("#%d: '%s' is a %s, child of #%d, at level %d.\n",
+        /*printf("#%d: '%s' is a %s, child of #%d, at level %d.\n",
             i, wi.wgt_name, wi.wgt_class, wi.parent_index,
-            wi.level);
+            wi.level);*/
         widgets[i] = wi;
         hierarchy[wi.level].push_back(i);
     }
     
-    typedef std::map<int, std::vector<ApDBWidgetInfo_t>/**/>::iterator hierarchy_iterator;
+    typedef std::map<int, std::vector<int>/**/>::iterator hierarchy_iterator;
+
     for (hierarchy_iterator it = hierarchy.begin(); it != hierarchy.end(); ++it)
     {
         ApDBWidgetInfo_t twi = widgets[it->first];
-        std::cout << "class " << twi.wgt_class << " " << twi.wgt_name << "{" << std::endl;
+        std::cout << std::string(it->first * 2, ' ') << "struct " << twi.wgt_class << " " << twi.wgt_name << "{" << std::endl;
 
         for (size_t i = 0; i < it->second.size(); ++i)
         {
-            ApDBWidgetInfo_t wi = it->second[i];
-            std::cout <<  wi.wgt_class << " " << twi.wgt_name << ";" << std::endl;
+            ApDBWidgetInfo_t wi = widgets[ it->second[i] ];
+            std::cout << std::string(it->first * 4, ' ') << wi.wgt_class << " " << twi.wgt_name << ";" << std::endl;
         }
 
-        std::cout << "};" << std::endl;
+        std::cout << std::string(it->first * 4, ' ') << "};" << std::endl;
     }
 
 
