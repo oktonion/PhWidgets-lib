d4db685e20bfd9fb677ee8e99869d05209cb108e
Alexander <0oktonion0@gmail.com>
2020-03-21 13:01:48 +0300

    + Pane widget

---
+++ src/Pane.cpp
@@ -0,0 +1,39 @@
+#include "Pane.h"
+
+#include <stdexcept>
+
+
+using namespace PhWidgets;
+
+namespace PhWidgets
+{
+    const char * WidgetClassName(PtWidget_t *wdg);
+}
+
+#define FORM_THROW_MESSAGE(xxx) (std::string("PhWidgets::") + std::string(#xxx": wrong class of photon widget - got \'") + WidgetClassName(widget()) + "\' instead of \'Pt"#xxx"\'.").c_str()
+#define WIDGET_IS_CLASS_MEMBER(xxx) \
+	if(PtWidgetIsClassMember( widget(), Pt##xxx ) != true)\
+		throw(std::invalid_argument(FORM_THROW_MESSAGE(xxx)));
+
+#define CHECK_WIDGET(xxx) \
+void xxx::check() \
+{ \
+	WIDGET_IS_CLASS_MEMBER(xxx); \
+}
+
+CHECK_WIDGET(Pane);
+
+
+Pane::Pane(int abn):
+	Container(abn)
+{
+	check();
+}
+
+Pane::Pane(PtWidget_t *wdg):
+	Container(wdg)
+{
+	check();
+}
+
+
